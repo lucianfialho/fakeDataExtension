d3cc68798180a768f8780bd100b6af54fd32c154
Lucian Fialho <lucianfialhobp@hotmail.com>
2021-05-17 09:33:49 -0300

    -- brecando na v3

--- background.js
+++ background.js
@@ -1,9 +1,29 @@
-chrome.contextMenus.create({
-    "id" : "Some id",
-    "title" : "My Context Menu",
-    "contexts": ["editable"]
-})
-  
-chrome.contextMenus.onClicked.addListener(function(clickData, tab){
-    console.log(clickData)
-})
+chrome.runtime.onInstalled.addListener(async () => {
+
+    chrome.contextMenus.create({
+        "id" : "Some id",
+        "title" : "My Context Menu",
+        "contexts": ["editable"]
+    })
+    
+    const currentTab = await getCurrentTab()
+
+    chrome.scripting.executeScript(
+    {
+        target: {tabId: currentTab.id},
+        files: ['script.js'],
+    });
+    chrome.contextMenus.onClicked.addListener(tab => {
+        
+    })
+});
+
+function showAlert() {
+    alert("test!");
+  }
+
+async function getCurrentTab() {
+    let queryOptions = { active: true, currentWindow: true };
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
+}
