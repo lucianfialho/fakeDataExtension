adac8ffa19ecd304a2d95d50e41df1b980877d09
Lucian Fialho <lucianfialhobp@hotmail.com>
2021-05-18 23:00:56 -0300

    -- add comunicacao do bg com o script

--- background.js
+++ background.js
@@ -8,7 +8,14 @@ chrome.contextMenus.create(menuItem);
 
 
 chrome.contextMenus.onClicked.addListener(function(info, tab){   
-    chrome.tabs.sendMessage(tab.id, "getClickedEl", {frameId: info.frameId}, data => {
-        elt.value = data.value;
+    chrome.tabs.sendMessage(tab.id, "getClickedEl", {frameId: info.frameId}, async (data) => {
+        let cpf = fetch("https://www.4devs.com.br/ferramentas_online.php", {
+            method: "POST",
+            headers: {'Content-Type': 'application/x-www-form-url-encoded', 'Accept': 'application/json'},
+            body: "acao=gerar_cpf&pontuacao=S&cpf_estado="
+        }).then(data => {
+            console.log(data)
+        });
+        console.log(cpf)
     });
 });
